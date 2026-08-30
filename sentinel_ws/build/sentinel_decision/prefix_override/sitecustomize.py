9e29ab542d02583c0afc7d45e9560a6539ba58d4
liulebujianle <hhhrrrppp123654@gmail.com>
2026-08-30 15:26:34 +0800

    lesson 15

--- sentinel_ws/build/sentinel_decision/prefix_override/sitecustomize.py
+++ sentinel_ws/build/sentinel_decision/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ethan/sentinel_ws/install/sentinel_decision'
+    sys.prefix = sys.exec_prefix = '/home/ethan/RoboVigor/sentinel_ws/install/sentinel_decision'
---
+++ sentinel_ws/build/sentinel_decision/share/sentinel_decision/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX//home/ethan/RoboVigor/sentinel_ws/build/sentinel_decision"
